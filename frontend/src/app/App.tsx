7712f7b93f5ffc800f4d9aca499dfc16fa49fd10
Mikhail Yastremsky <maukul06@gmail.com>
2023-01-22 20:19:11 +0200

    build(frontend): add eslint and prettier

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,9 +1,9 @@
-import { withProviders } from './providers'
-import './index.css'
-import { Routing } from '../pages'
+import { withProviders } from "./providers";
+import "./index.css";
+import { Routing } from "../pages";
 
 const App = () => {
-  return <div className='app'>{<Routing />}</div>
-}
+  return <div className="app">{<Routing />}</div>;
+};
 
-export default withProviders(App)
+export default withProviders(App);
