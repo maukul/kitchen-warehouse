b598e227715406e119d07d8c8c9d0163bbb0cd73
Mikhail Yastremsky <maukul06@gmail.com>
2023-02-28 08:13:00 +0200

    feat(shared-constants): add links

---
+++ frontend/src/shared/constants/links.ts
@@ -0,0 +1,9 @@
+export const links = {
+  reports: '/reports',
+  menu: '/menu',
+  templatesMenu: '/templates-menu',
+  dishes: '/dishes',
+  warehouses: '/warehouses',
+  types: '/types',
+  login: '/login',
+} as const;
